de0b2709aa1c968b0bd251f017481d9c564a20c6
Jady Rufino <jadyrufa@gmail.com>
2021-05-03 22:06:15 -0300

    style: home and table style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,149 @@
-import { Header } from "../components/Header";
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
+import { api } from '../services/api'
 
-export default function Home(props) {
-  return (
-    <div>
-      <h1>index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
-    </div>
-    )
+import { format, parseISO } from 'date-fns';
+import ptBR  from 'date-fns/locale/pt-BR';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+import styles from './home.module.scss';
+
+type Episodes = {
+  id: string,
+  title: string,
+  thumbnail: string,
+  description: string,
+  members: string,
+  duration: number,
+  durationAsString: string,
+  url: string,
+  publishedAt: string,
 }
 
-// Exemplo de SPA
+type HomeProps = {
+  latestEpisodes: Episodes[]  // ou Array<Episodes>
+  allEpisodes: Episodes[]  // ou Array<Episodes>
+}
 
+export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
+  return (
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos Episódios</h2>
 
-// Exemplo chamada SSR
-// Nexte já indentifica precisa chamar essa função antes de exibir a página.
-// Usado TODA VEZ que o usário usar a Home da aplicação
-/* export async function getServerSideProps(){
-  const response = await fetch('http://localhost:3333/episodes');
+        <ul>
+          {latestEpisodes.map(episode => {
+            return(
+              <li key={episode.id}>
+                <Image 
+                  width={192}
+                  height={192}
+                  src={ episode.thumbnail } 
+                  alt={ episode.title } 
+                  objectFit="cover"
+                />
+                
+                <div className={styles.episodeDetails}>
+                  <a href="">{episode.title}</a>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
 
-  const data = await response.json();
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar Episódio"/>
+                </button>
+
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+      
+      <section className={styles.allEpisodes}>
+          <h2>Todos Episódios</h2>
+
+          <table cellSpacing={0}>
+            <thead>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </thead>
+            <tbody>
+              {allEpisodes.map(episode => {
+                return (
+                  <tr key={episode.id}>
+                    <td style={{ width: 72}}>
+                      <Image 
+                        width={120}
+                        height={120}
+                        src={episode.thumbnail}
+                        alt={episode.title}
+                        objectFit="cover"
+                      />
+                    </td>
+                    <td>
+                      <a href="">{episode.title}</a>
+                    </td>
+                    <td>{episode.members}</td>
+                    <td style={{width: '100px'}}>{episode.publishedAt}</td>
+                    <td>{episode.durationAsString}</td>
+                    <td>
+                      <button type="button">
+                        <img src="/play-green.svg" alt="Tocar episódio"/>
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+      </section>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  // const response = await api.get('http://localhost:3333/episodes?_limit=12&_sort=published_at&order=desc');
+
+  const { data } = await api.get('episodes', {
+    params: {
+      limit: 12,
+      sort: 'published_at',
+      order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
+    }
+  })
+
+  const latestEpisodes = episodes.slice(0, 2)
+  const allEpisodes = episodes.slice(2, episodes.length)
 
   return {
     props: {
-      episodes: data,
-    }
-  }
-} */
-
-
-
-// Exemplo de SSG
-
-//A diferença está na chamada do método StaticSiteProps e a opção revalidate que informa o tempo que a página irá sofrer uma atualização
-// Precisa gerar uam build em produção
-export async function StaticSiteProps(){
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
-
-  return { 
-    props: {
-      episodes: data,
+      episodes,
+      latestEpisodes, 
+      allEpisodes
     },
+
     revalidate: 60 * 60 * 8,
   }
+
 
 }
 
